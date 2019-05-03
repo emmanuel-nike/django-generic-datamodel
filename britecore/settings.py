c8e01737cae67a3e3e29e29e1acb082535a80085
Emmanuel Nwankwo <nike.applications@gmail.com>
2019-05-03 13:06:37 +0100

    heroku config

--- britecore/settings.py
+++ britecore/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/2.2/ref/settings/
 """
 
 import os
+import django_heroku
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -136,3 +137,6 @@ MEDIA_URL = '/media/'
 STATICFILES_DIRS = [
     STATIC_DIRS,
 ]
+
+# Activate Django-Heroku.
+django_heroku.settings(locals())
